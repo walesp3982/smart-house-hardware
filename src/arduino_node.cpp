aae89b90eec9b54a74ff4620a923b063a11ed1f3
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-03 14:28:14 -0400

    corrigiendo librería platformio.ini

--- src/arduino_node.cpp
+++ src/arduino_node.cpp
@@ -1,14 +1,46 @@
-#include <Wire.h>
-#include <Arduino.h>
+#include<Arduino.h>
+#include<Servo.h>
+
+Servo puertaPrincipal;
+Servo garaje;
+Servo dormitorio;
 
 void setup() {
-    Wire.begin(); 
-    int a = 5;
+  Serial.begin(9600);
+
+  puertaPrincipal.attach(3);
+  garaje.attach(5);
+  dormitorio.attach(6);
+  Serial.print("Hola a todo el mundo");
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 void loop() {
-    Wire.beginTransmission(8);
-    Wire.write("ON");
-    Wire.endTransmission();
-    delay(1000);
+  if (Serial.available()) {
+    String comando = Serial.readStringUntil('\n');
+
+    if (comando == "PUERTA") {
+      puertaPrincipal.write(90);
+    }
+
+    if (comando == "GARAJE") {
+      garaje.write(90);
+    }
+
+    if (comando == "DORMITORIO") {
+      dormitorio.write(90);
+    }
+
+    if (comando == "LUZ_GARAJE") {
+      digitalWrite(8, HIGH);
+    }
+
+    if (comando == "LUZ_SALA_COCINA") {
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+    }
+  }
 }
