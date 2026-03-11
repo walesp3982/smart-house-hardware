fa53b84831db7f42bdf0bfb894e81c67f24d03c1
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-10 20:22:42 -0400

    refactorización de pines de cam, y creación de variables de entorno para las credenciales del wifi

---
+++ master_esp32/load_env.py
@@ -0,0 +1,8 @@
+import os
+
+with open('.env', 'r') as f:
+    for line in f:
+        if line.strip() and not line.startswith('#'):
+            key, value = line.strip().split('=', 1)
+            os.environ[key] = value
+            print(f'-D{key.strip()}=\\"{value.strip()}\\"')
